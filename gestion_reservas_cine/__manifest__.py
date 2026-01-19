be266011cf472aed2bc8d3ab33df6d0cdce09e82
randomstudentES <154506723+randomstudentES@users.noreply.github.com>
2026-01-19 20:18:08 +0100

    Update __manifest__.py

--- gestion_reservas_cine/__manifest__.py
+++ gestion_reservas_cine/__manifest__.py
@@ -1,24 +1,27 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "gestion_reservas_cine",
+    'name': "Gestion Reservas Cine",
 
-    'summary': "Short (1 phrase/line) summary of the module's purpose",
+    'summary': "Módulo para la gestión de reservas en el cine",
 
     'description': """
-Long description of module's purpose
+Módulo de gestión de reservas para un cine.
+Permite gestionar servicios, clientes y reservas,
+incluyendo cálculo automático de precios y futura
+integración con facturación
     """,
 
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
+    'author': "Javier, Fran y Andrii",
+    'website': "https://github.com/N16K-ed/Modulo_SGEM_EV2",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Services',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'contacts', 'account'],
 
     # always loaded
     'data': [
@@ -30,5 +33,8 @@ Long description of module's purpose
     'demo': [
         'demo/demo.xml',
     ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
 }
 
